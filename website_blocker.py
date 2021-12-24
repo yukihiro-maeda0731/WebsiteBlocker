cadde1d7ca38fb4841d8a19eade3e4e0c12656e2
前田 <sloth19920731@gmail.com>
2021-12-24 21:56:02 +0900

    ３２２最後まで:datetimeで条件分岐

--- website_blocker.py
+++ website_blocker.py
@@ -1,8 +1,13 @@
 import time
+from datetime import datetime as dt
+
 hosts_path="C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["https://www.amazon.co.jp/"]
 
 while True:
-    print(1)
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23):
+        print("Working hours...")
+    else:
+        print("Fun hours...")
     time.sleep(5)
