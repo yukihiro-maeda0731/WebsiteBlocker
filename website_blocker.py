c4f6a476df7dea8aedaf49a9810077e087d19b3d
前田 <sloth19920731@gmail.com>
2021-12-25 23:35:46 +0900

    323の6:50あたりまで:Must have exactly one of create/read/write/append mode and at most one plus

--- website_blocker.py
+++ website_blocker.py
@@ -1,13 +1,18 @@
 import time
 from datetime import datetime as dt
 
+hosts_temp="hosts"
 hosts_path="C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["https://www.amazon.co.jp/"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23):
+    # if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,23):
+    if dt.now() != dt(dt.now().year,dt.now().month,dt.now().day,23):
         print("Working hours...")
+        with open(hosts_temp,'') as file:
+            content=file.read()
+            print(content)
     else:
         print("Fun hours...")
     time.sleep(5)
